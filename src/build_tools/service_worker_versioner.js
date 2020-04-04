18992d647cd6904c478a920f4ec35b1e2ba485eb
Ovidiu Ionescu <ovidiu@ionescu.net>
2020-04-04 17:07:37 +0200

    chore: tha list of files in the service worker is generated at build time

--- src/build_tools/service_worker_versioner.js
+++ src/build_tools/service_worker_versioner.js
@@ -1,18 +1,63 @@
+/**
+ * Replaces cache version in the service worker so we get a fresh one at 
+ * every build.
+ * Regenerates the list of files cached by the service worker.
+ */
+
 const fs = require ('fs');
 
-
 const version_file = 'src/build_tools/version_counter.txt'
-const files = ['build/main/worker/service-worker.js'];
-const marker = '{AUTOINCREMENT_CACHE_VERSION}';
+const src_dir = 'build_worker';
+const build_dir = 'build'
+const files = ['main/worker/service-worker.js'];
+const dirs = ['src/main/dom', 'src/main/pkg', 'images'];
+const version_marker = '{AUTOINCREMENT_CACHE_VERSION}';
+const all_source_marker = '"{ALL_SOURCES}"';
+const exclude = new Set(['memo_interfaces.ts', 'tulip-blue.svg', 'tulip-green.svg']);
 
 
-let version = parseInt(fs.readFileSync(version_file, 'utf8'), 10) + 1;
-console.log('Incrementing versions to', version);
+const get_next_version = () => {
+  let version = parseInt(fs.readFileSync(version_file, 'utf8'), 10) + 1;
+  console.log('Incrementing versions to', version);
+  
+  fs.writeFileSync(version_file, version);
 
-fs.writeFileSync(version_file, version);
+  return version;
+}
 
-files.forEach(f => {
-  let content = fs.readFileSync(f, 'utf8');
-  content = content.replace(marker, version);
-  fs.writeFileSync(f, content);
-});
+const scan_dir = (dir, exclude) => {
+  const suf = dir.split('/').pop();
+  return fs.readdirSync(dir)
+    .filter(f => f.match(/.+\.(ts|js|svg|wasm)$/))
+    .filter(f => !exclude.has(f))
+    .filter(f => !f.match(/.+\.d\.ts$/))
+    .map(f => f.replace(/\.ts$/, '.js'))
+    .map(f => `    "${suf}/${f}",\n`)
+    .reduce((acc, v) => acc + v);
+};
+
+const get_all_files = () => {
+  return dirs
+    .map(d => scan_dir(d, exclude))
+    .reduce((acc, v) => acc + v).slice(4, -2);
+};
+
+const populate_markers = () => {
+  return new Map([
+    [version_marker, get_next_version()],
+    [all_source_marker, get_all_files()], 
+  ]);
+}
+
+const process_files = (markers) => {
+  files.forEach(f => {
+    let content = fs.readFileSync(`${src_dir}/${f}`, 'utf8');
+
+    markers.forEach((value, key) => {
+      content = content.replace(key, value);
+    });
+    fs.writeFileSync(`${build_dir}/${f}`, content);
+  });
+}
+
+process_files(populate_markers());
